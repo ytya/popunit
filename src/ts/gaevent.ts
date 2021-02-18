3f766d9deb0bd4153752e22f2e47bac871d8e53f
ytya <ytyadev@gmail.com>
2021-02-18 22:04:02 +0900

    [fix] GAイベント修正

--- src/ts/gaevent.ts
+++ src/ts/gaevent.ts
@@ -13,18 +13,11 @@ export class GAEvent {
   static updateTable(brands: string[], attr1: string, attr2: string, attr3: string, refineWords: string[]): void {
     if (!window.gtag) return
 
-    // 検索条件をsearch_termに詰めて送信
-    let term = ''
-    for (const brand of brands) {
-      term += `brand:${brand} `
-    }
-    term += `attr1:${attr1} `
-    term += `attr2:${attr2} `
-    term += `attr3:${attr3}`
-    for (const word of refineWords) {
-      term += ` word:${word}`
-    }
-
-    window.gtag('event', 'search', { search_term: term })
+    // 検索条件を送信
+    window.gtag('event', 'fetch_unit', {
+      cond_brand: brands.join(' '),
+      cond_attr: [attr1, attr2, attr3].sort().join(''),
+      cond_refine: refineWords.join(' '),
+    })
   }
 }
